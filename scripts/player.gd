637d93fce7ef29e043347769e21401712f30ab15
Xwilarg <xwilarg@protonmail.com>
2023-03-04 18:36:32 +0100

    Add ability to throw net

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,9 +12,7 @@ var thrust = 100.0
 @onready var sprite = %Sprite2D
 var x_direction
 
-
-func _ready():
-	pass
+@export var net: PackedScene
 
 
 func _process(_delta):
@@ -25,6 +23,14 @@ func _process(_delta):
 		sprite.flip_h = false
 	elif (not sprite.flip_h) and x_direction < 0:
 		sprite.flip_h = true
+
+	if Input.is_action_just_pressed("fire"):
+		var go = net.instantiate()
+		get_parent().add_child(go)
+		go.position = position
+		var x = -1 if sprite.flip_h else 1
+		(go as RigidBody2D).add_constant_central_force(Vector2(3 * x, -1).normalized() * 350)
+		(go.get_node("Sprite2D") as Sprite2D).flip_h = x
 
 
 func _physics_process(delta):
