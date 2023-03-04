f4c2b78673e58fffc009183fd97593ccda86270a
Xwilarg <xwilarg@protonmail.com>
2023-03-04 15:26:39 +0100

    Fix bug in node referencing

--- scripts/NextNode.gd
+++ scripts/NextNode.gd
@@ -3,17 +3,23 @@ extends Node2D
 
 class_name NextNode
 
-@export var Next: Array[String]
+@export var Next: Array[String] # Thanks Godot https://github.com/godotengine/godot/issues/62916
 
+# Get a node given its index
 func indexToNode(i):
 	return get_parent().get_node("AIMovNode" + Next[i])
 
+# Return a random next node
 func getRandomNext():
 	return indexToNode((get_tree().root.get_child(0).get_node("RNG") as RNG).random(Next.size()))
 
 func _ready():
+	# For each next node, we add ourself to it
+	var me = name.right(name.length() - 9)
 	for i in range(0, Next.size()):
-		(indexToNode(i) as NextNode).Next.push_back(self)
+		var node = (indexToNode(i) as NextNode)
+		if !node.Next.has(me):
+			node.Next.push_back(me)
 		
 
 func _draw():
