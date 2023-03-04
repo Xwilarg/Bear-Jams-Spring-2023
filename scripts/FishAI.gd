c3ff6290340b3424d570c60c5548e6e2bb2fb614
Xwilarg <xwilarg@protonmail.com>
2023-03-04 15:29:43 +0100

    Flip fish sprite when needed

--- scripts/FishAI.gd
+++ scripts/FishAI.gd
@@ -5,11 +5,14 @@ extends RigidBody2D
 @export var MinDistance: int
 
 var next: NextNode
+var sr: Sprite2D
 
 func distanceComparaison(a: Node2D, b: Node2D):
 	return position.distance_to(a.position) < position.distance_to(b.position)
 
 func _ready():
+	sr = $"./Sprite2D"
+
 	var targetNodes = NodeContainer.get_children()
 	targetNodes.sort_custom(distanceComparaison)
 	next = targetNodes[0]
@@ -18,3 +21,4 @@ func _integrate_forces(state):
 	linear_velocity = (next.position - position).normalized() * Speed
 	if position.distance_to(next.position) < MinDistance:
 		next = next.getRandomNext()
+	sr.flip_h = linear_velocity.x < 0
