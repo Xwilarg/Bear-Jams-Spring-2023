f720fca7df2f6b82bba0bcd9b3ca4a256388e4f9
Kirito <51691757+KoB-Kirito@users.noreply.github.com>
2023-03-05 17:30:38 +0100

    fixes pressure in level 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -46,7 +46,7 @@ func _process(delta):
 		linear_velocity = Vector2.ZERO
 	
 	# pressure = depth?
-	pressure = global_position.y * 0.1
+	pressure = (global_position.y + 1000) * 0.02
 	
 	# animation
 	if Input.is_action_pressed("move_down") \
