b95823bc9c462befaa667aaa367f1888236fc7b9
Xwilarg <xwilarg@protonmail.com>
2023-03-05 01:03:31 +0100

    Add credits

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@export var credits: Panel
 var level1 = preload("res://scenes/game/levels/level1.tscn")
 
 func _ready():
@@ -8,3 +8,11 @@ func _ready():
 
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(level1)
+
+
+func _on_credits_pressed():
+	credits.visible = true
+
+
+func _on_credits_x_pressed():
+	credits.visible = false
